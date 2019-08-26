c81b9ae1e7587f0a7f4896f008f144006b291121
TheGod007 <1279946204@qq.com>
2019-08-26 21:55:12 +0800

    second commit

--- src/com/itheima/git/FirstClass.java
+++ src/com/itheima/git/FirstClass.java
@@ -7,5 +7,6 @@ public class FirstClass {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
